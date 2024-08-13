b00b699e1302db2654ac52a342f3b68089cafbf6
tristanpersonal <tristanstephennido3@gmail.com>
2024-08-13 23:39:53 +0800

    Update controlandscroll.js

--- js/controlandscroll.js
+++ js/controlandscroll.js
@@ -1,23 +1,3 @@
-function showAbout(optionNum){
-    const optionTab = document.getElementsByClassName('optionTab');
-    const aboutInfo = document.getElementsByClassName('aboutInfo'); 
-    const aboutPicture = document.getElementsByClassName('aboutPicture');
-
-    for(let i=0; i<aboutInfo.length; i++){
-        if(i === optionNum){
-            aboutInfo[i].style.display = 'block';
-            aboutPicture[i].style.display = 'block';
-            optionTab[i].style.color = 'white';
-            optionTab[i].style.fontWeight = '600';
-        }else{
-            aboutInfo[i].style.display = 'none';
-            aboutPicture[i].style.display = 'none';
-            optionTab[i].style.color = 'gray';
-            optionTab[i].style.fontWeight = 'normal';
-        }
-    }
-}
-
 const createObserver = (hiddenClass, showClass) => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
